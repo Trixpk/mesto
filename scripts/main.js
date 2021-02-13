7b6f496536e0ec0007e3624af5e258ff7f5d0123
Pavel Poliakov <89504082116@mail.ru>
2021-02-13 15:08:30 +0700

    добавлена валидация форм и обработка закрытия кнопкой ESC

--- scripts/main.js
+++ scripts/main.js
@@ -45,9 +45,7 @@ function toggleModal(element) {
     element.classList.toggle('popup_opened');
 }
 
-function savePopup(event) {
-    event.preventDefault();
-
+function savePopup() {
     profileName.textContent = nameInput.value;
     profileProfession.textContent = professionInput.value;
 
@@ -65,7 +63,7 @@ function createCard(link, name) {
     newCardTitle = newCardName.textContent = name;
 
     newCardImg.addEventListener('click', function (evt) {
-        if(evt.target.classList.contains('cards__img')) {
+        if (evt.target.classList.contains('cards__img')) {
             cardDetailImg.src = evt.target.src;
             cardDetailTitle.textContent = name;
 
@@ -78,15 +76,13 @@ function createCard(link, name) {
     });
 
     newCardButtonLike.addEventListener('click', function () {
-       newCardButtonLike.classList.toggle('cards__like_active');
+        newCardButtonLike.classList.toggle('cards__like_active');
     });
 
     return newCard;
 }
 
 function addCard(event) {
-    event.preventDefault();
-
     const cardElement = createCard(cardLink.value, cardName.value);
     cardsSection.prepend(cardElement);
     toggleModal(popupAddForm);
@@ -100,11 +96,11 @@ initialCards.forEach(function (card) {
 });
 
 popups.forEach((popup) => {
-   popup.addEventListener('click', (evt) => {
-      if(evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__close-button')) {
-          toggleModal(popup);
-      }
-   });
+    popup.addEventListener('click', (evt) => {
+        if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__close-button')) {
+            toggleModal(popup);
+        }
+    });
 });
 
 editButton.addEventListener('click', function () {
@@ -118,3 +114,78 @@ addButton.addEventListener('click', function () {
 popupEditForm.addEventListener('submit', savePopup);
 
 popupAddForm.querySelector('.popup__form').addEventListener('submit', addCard);
+
+// validation
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+    });
+};
+
+const toggleButtonState = (inputList, button) => {
+    if(hasInvalidInput(inputList)) {
+        button.classList.add('popup__submit_inactive');
+    }else {
+        button.classList.remove('popup__submit_inactive');
+    }
+};
+
+const setEventListeners = (form) => {
+    const fieldList = Array.from(form.querySelectorAll('.popup__field'));
+    const button = form.querySelector('.popup__submit');
+    toggleButtonState(fieldList, button);
+
+    fieldList.forEach((field) => {
+        field.addEventListener('input', () => {
+            checkInputValidity(form, field);
+            toggleButtonState(fieldList, button);
+        });
+    });
+};
+
+const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll('.form'));
+    formList.forEach((formElement) => {
+        formElement.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+        setEventListeners(formElement);
+    });
+};
+
+enableValidation();
+
+const showInputError = (form, input, errorMessage) => {
+    const errorElement = form.querySelector(`.${input.id}-error`);
+    input.classList.add('popup__field_type_error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__input-error_active');
+};
+
+const hideInputError = (form, input) => {
+    const errorElement = form.querySelector(`.${input.id}-error`);
+    input.classList.remove('popup__field_type_error');
+    errorElement.classList.remove('popup__input-error_active');
+    errorElement.textContent = '';
+};
+
+const checkInputValidity = (form, input) => {
+    if(!input.validity.valid) {
+        showInputError(form, input, input.validationMessage);
+    }else {
+        hideInputError(form, input);
+    }
+};
+
+const closePopupEsc = () => {
+    document.addEventListener('keydown', (evt) => {
+        if(evt.key === 'Escape') {
+            const openedForm = document.querySelector('.popup_opened');
+            if(openedForm !== null) {
+                toggleModal(openedForm);
+            }
+        }
+    });
+};
+
+closePopupEsc();
